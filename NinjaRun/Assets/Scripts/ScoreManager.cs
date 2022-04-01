37eef489fdbfb7ffc8c540541ad9192ec42d6285
Subash <it.subashaa@gmail.com>
2022-04-01 23:22:10 +0530

    Highscore

--- NinjaRun/Assets/Scripts/ScoreManager.cs
+++ NinjaRun/Assets/Scripts/ScoreManager.cs
@@ -5,10 +5,35 @@ public class ScoreManager : MonoBehaviour
 {
     [SerializeField] Transform player;
     [SerializeField] Text scoreText;
+    [SerializeField] Text highScoreText;
+
+    int highScore = 0;
+
+    private void Start()
+    {
+        highScore = PlayerPrefs.GetInt("HighScore", 0);
+        highScoreText.text = "/" + highScore.ToString();
+    }
 
     private void Update()
     {
+        updateScore();
+        updateHighScore();
+    }
+
+    void updateScore()
+    {
         scoreText.text = player.position.z.ToString("0");
+    }
+    void updateHighScore()
+    {
+        int score = int.Parse(scoreText.text);
+
+        if (highScore < score)
+        {
+            PlayerPrefs.SetInt("HighScore", score);
+            highScoreText.text = "/" + score.ToString();
+        }
     }
 
 }
